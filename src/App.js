c92126146c0678b227aefebbecea31dd6dae3fef
Hyejin Eom <yazbyz@gmail.com>
2021-10-04 14:24:17 +0900

    [ADD] main, newPost, post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Main from "./js/main/main";
+import Post from "./js/post/post";
+import newPost from "./js/post/newPost";
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/posts" component={Main}/>
+          <Route path="/posts/:postId" component={Post}/>
+          <Route path="/new" component={newPost}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
