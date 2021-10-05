e3aa33cfc66d5f94a1b754e3e957e79e8412f702
devGO <been2zz@naver.com>
2021-10-05 22:46:24 +0900

    [UPDATE] MEMO LIST

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,9 +1,30 @@
 import React, { useState, useEffect }  from "react";
 import axios from 'axios';
+import Post from "./post.js";
+import { CardGroup } from "react-bootstrap";
 
 function Main() {
+    const [posts, setPost] = useState("");
+
+    useEffect(() =>{
+        axios.get("http://localhost:3001/posts")
+            .then(res => setPost(res.data))
+    }, []);
+
     return (
-        <div>
+        <div className="container">
+        <CardGroup>
+            {[...posts].map(post => (
+                <Post
+                    key={post.id}
+                    id={post.id}
+                    title={post.title}
+                    body={post.body}
+                    date={post.published_at}
+                    author={post.author}
+                />
+            ))}
+        </CardGroup>
         </div>
     );
 }
