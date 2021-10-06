fb8a1b75314ee9d3ad96cd1457ec495e4fa5280d
devGO <been2zz@naver.com>
2021-10-07 00:37:08 +0900

    [ADD] Pagination

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,20 +1,35 @@
 import React, { useState, useEffect }  from "react";
 import axios from 'axios';
 import Post from "./post.js";
+import Paging from './paging';
 import { CardGroup } from "react-bootstrap";
 
 function Main() {
     const [posts, setPost] = useState("");
-
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 12;
+    console.log(posts.length)
+    
     useEffect(() =>{
-        axios.get("http://localhost:3001/posts")
-            .then(res => setPost(res.data))
+        async function fetchDate() {
+            const response = await axios.get("http://localhost:3001/posts");
+            setPost(response.data);
+        }
+        fetchDate();
     }, []);
 
+    // Paging
+    const indexOfLast = currentPage * postsPerPage;
+    const indexOfFirst = indexOfLast - postsPerPage;
+    function currentPosts(tmp) {
+        let currentPosts = [];
+        currentPosts = tmp.slice(indexOfFirst, indexOfLast);
+        return [...currentPosts];
+    }
     return (
         <div className="container">
         <CardGroup>
-            {[...posts].map(post => (
+            {currentPosts(posts).map(post => (
                 <Post
                     key={post.id}
                     id={post.id}
@@ -25,6 +40,7 @@ function Main() {
                 />
             ))}
         </CardGroup>
+        <Paging postsPerPage={postsPerPage} totalPosts={posts.length} currentPage={currentPage} paginate={setCurrentPage}></Paging>
         </div>
     );
 }
